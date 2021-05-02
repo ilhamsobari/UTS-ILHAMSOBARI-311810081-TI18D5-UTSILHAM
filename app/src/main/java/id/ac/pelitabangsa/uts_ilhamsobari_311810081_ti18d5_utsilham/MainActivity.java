b960382e01457e4408d9fe61fcc7396b70059eac
ilhamsobari <ilhamshobari25@gmail.com>
2021-05-02 16:52:50 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/pelitabangsa/uts_ilhamsobari_311810081_ti18d5_utsilham/MainActivity.java
@@ -0,0 +1,64 @@
+package id.ac.pelitabangsa.uts_ilhamsobari_311810081_ti18d5_utsilham;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startBtn = (Button) findViewById(R.id.sendEmail);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendEmail();
+            }
+        });
+    }
+
+    public void panggil(View view) {
+        String nomor = "082299010238";
+        Intent panggil = new Intent(Intent.ACTION_DIAL);
+        panggil.setData(Uri.fromParts("tel", nomor, null));
+        startActivity(panggil);
+    }
+
+    public void buka(View view) {
+        String url = "https://www.shopee.com";
+        Intent bukabrowser = new Intent(Intent.ACTION_VIEW);
+        bukabrowser.setData(Uri.parse(url));
+        startActivity(bukabrowser);
+    }
+
+    public void skuylah(View view) {
+        String url = "https://www.google.co.id/maps/@-6.2721002,107.3257548,15z";
+        Intent maps = new Intent(Intent.ACTION_VIEW);
+        maps.setData(Uri.parse(url));
+        startActivity(maps);
+    }
+
+    protected void sendEmail(){
+        Log.i("Send email","");
+        String[] To = {""};
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, To);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT,"Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+        startActivity(emailIntent);
+    }
+
+}
---
+++ app/src/test/java/id/ac/pelitabangsa/uts_ilhamsobari_311810081_ti18d5_utsilham/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package id.ac.pelitabangsa.uts_ilhamsobari_311810081_ti18d5_utsilham;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
